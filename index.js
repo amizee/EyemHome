77126533df21dda441810cbde7d3bfa2c6949483
Kevin Kong <kkon0188@uni.sydney.edu.au>
2024-07-11 06:27:23 +1000

    home screen

--- index.js
+++ index.js
@@ -1,23 +1,103 @@
 import {SvgPlus, SquidlyApp} from "https://session-app.squidly.com.au/src/Apps/app-class.js"
 
-class BedroomWindow extends SvgPlus {
-    constructor(editable, app){
-        super ("div");
-        this.app = app;
-        this.styles = {
-            "position":  "absolute"
-        }
-        // this.innerHTML = "hello";
-        this.createChild("img", {src: "http://127.0.0.1:5502/images/icon.png", styles: {width: "100%", height: "100%"}});
-    }
+class MainWindow extends SvgPlus {
+    constructor(isSender, app) {
+        super("div");
 
+        this.styles = {
+            "position": "absolute",
+            "display": "flex",
+            "justify-content": "center", 
+            "align-items": "center", 
+            "width": "80%",
+            "height": "80%",
+            "top": "50%",
+            "left": "50%",
+            "transform": "translate(-50%, -50%)"
+        }
+
+        let homeDiv = this.createChild("div");
+        homeDiv.styles = {
+            "position": "relative"
+        }
+
+        let house = this.createChild("img", {src: "http://127.0.0.1:5502/images/house.png"});        
+        let bedroom = this.createChild("img", {
+            src: "http://127.0.0.1:5502/images/bedroom.png", 
+            styles: {
+                position: "absolute",
+                height: "18.5%",
+                top: "68.1%",
+                right: "34.5%"
+            }
+        });
+        homeDiv.appendChild(house);
+        homeDiv.appendChild(bedroom);
+        
+        bedroom.addEventListener('mouseover', () => {
+            bedroom.styles = {cursor: "pointer"};
+        })
+        bedroom.addEventListener('mouseout', () => {
+            bedroom.styles = {cursor: "auto"};
+        })
+        
+        let hoverTimer;
+        bedroom.addEventListener('mouseenter', function(e) {
+            let progressBar = new ProgressBar(e);
+            progressBar.props = {class: "progress-bar"};
+            document.body.appendChild(progressBar);
+            progressBar.animate();
+
+            hoverTimer = setTimeout(() => {
+                homeDiv.styles = {display: "none"};
+            }, 1000);
+        });
+
+        bedroom.addEventListener('mouseleave', function() {
+            let progressBars = document.querySelectorAll('.progress-bar');
+            for (let i = 0; i < progressBars.length; i++) {
+                progressBars[i].remove();
+            };
+            clearTimeout(hoverTimer);
+        });
+    }    
 }
 
-export default class testApp extends SquidlyApp {
+class ProgressBar extends SvgPlus {
+    constructor(e) {
+        super("div");
+
+        this.styles = {
+            position: "absolute",
+            left: e.clientX + "px",
+            top: e.clientY + "px",
+            width: "75px",
+            height: "75px",
+            "border-radius": "50%",
+            background: "radial-gradient(closest-side, white 69%, transparent 70% 100%), conic-gradient(limegreen calc(var(--progress-value) * 1%), lightgreen 0)",
+        }
+    }
+
+    animate() {
+        let progressValue = 0;
+
+        // Increment progress every 10 milliseconds
+        let interval = setInterval(() => {
+            if (progressValue >= 100) {
+                clearInterval(interval);
+            } else {
+                progressValue++;
+                this.style.setProperty('--progress-value', progressValue);
+            }
+        }, 10);
+    }
+}
+
+export default class TestApp extends SquidlyApp {
     constructor(isSender, initializer){
         super(isSender, initializer);
 
-        this.window = new BedroomWindow(isSender, this);
+        this.window = new MainWindow(isSender, this);
     }
 
     getMainWindow(){
