ae0079deaacda307c50e03bdef559d13a1a25a46
Kevin Kong <kkon0188@uni.sydney.edu.au>
2024-07-14 23:59:25 +1000

    slight improvements

--- index.js
+++ index.js
@@ -16,24 +16,28 @@ class MainWindow extends SvgPlus {
             "transform": "translate(-50%, -50%)"
         }
 
-        let homeDiv = this.createChild("div");
-        homeDiv.styles = {
-            "position": "relative"
-        }
+        let audio = this.createChild("audio", {src: "http://127.0.0.1:5502/sounds/home.mp3"});
 
-        let house = this.createChild("img", {src: "http://127.0.0.1:5502/images/house.png"});        
-        let bedroom = this.createChild("img", {
+        let homeDiv = this.createChild("div", {styles: {position: "relative"}});
+        let volumeButton = new VolumeButton(audio); // Some sort of toolbar?
+        homeDiv.appendChild(volumeButton);
+
+        let house = this.createChild("img", {src: "http://127.0.0.1:5502/images/house.png"}); // To stack properly, have to use media queries, fixed size for house or cover the windows completely  
+        let bedroom = this.createChild("img", { 
             src: "http://127.0.0.1:5502/images/bedroom.png", 
             styles: {
                 position: "absolute",
-                height: "18.5%",
-                top: "68.1%",
-                right: "34.5%"
+                height: "20%",
+                top: "67%",
+                right: "33.7%",
+                border: "solid 8px #466596"
             }
         });
+
         homeDiv.appendChild(house);
         homeDiv.appendChild(bedroom);
-        
+        app.set("room", "home"); // Reload goes to home page
+
         bedroom.addEventListener('mouseover', () => {
             bedroom.styles = {cursor: "pointer"};
         })
@@ -41,15 +45,23 @@ class MainWindow extends SvgPlus {
             bedroom.styles = {cursor: "auto"};
         })
         
+        app.onValue("room", (value) => { // Triggers once immediately after the listener is attached, to provide the current value of the data
+            if (value === "home") {
+                homeDiv.styles = {display: "block"}
+            }
+            else if (value === "levels") {
+                homeDiv.styles = {display: "none"};
+            }
+        });
+
         let hoverTimer;
         bedroom.addEventListener('mouseenter', function(e) {
             let progressBar = new ProgressBar(e);
             progressBar.props = {class: "progress-bar"};
             document.body.appendChild(progressBar);
             progressBar.animate();
-
             hoverTimer = setTimeout(() => {
-                homeDiv.styles = {display: "none"};
+                app.set("room", "levels");
             }, 1000);
         });
 
@@ -80,7 +92,6 @@ class ProgressBar extends SvgPlus {
 
     animate() {
         let progressValue = 0;
-
         // Increment progress every 10 milliseconds
         let interval = setInterval(() => {
             if (progressValue >= 100) {
@@ -93,6 +104,41 @@ class ProgressBar extends SvgPlus {
     }
 }
 
+class VolumeButton extends SvgPlus {
+    constructor(audio) {
+        super("img");
+
+        this.props = {src: "http://127.0.0.1:5502/images/volume-mute.png"};
+        this.styles = {
+            position: "absolute",
+            width: "64px", 
+            height: "64px",
+            top: "-6%",
+            right: "3%"
+        }
+
+        this.addEventListener('click', function() {
+            if (audio.muted) {
+                this.src = "http://127.0.0.1:5502/images/volume.png";
+                audio.muted = false;
+                audio.load();
+                audio.loop = true;
+                audio.play();
+            } else {
+                this.src = "http://127.0.0.1:5502/images/volume-mute.png";
+                audio.muted = true;
+            }
+        })
+
+        this.addEventListener('mouseover', () => {
+            this.styles = {cursor: "pointer"};
+        })
+        this.addEventListener('mouseout', () => {
+            this.styles = {cursor: "auto"};
+        })
+    }
+}
+
 export default class TestApp extends SquidlyApp {
     constructor(isSender, initializer){
         super(isSender, initializer);
@@ -100,19 +146,17 @@ export default class TestApp extends SquidlyApp {
         this.window = new MainWindow(isSender, this);
     }
 
-    getMainWindow(){
+    getMainWindow() {
         return this.window;
     }
 
-    get name(){
+    static get name() {
         return "MyApp"
     }
 
-    get appIcon(){
-        let icon = new SvgPlus("div");
-        // appicon.createChild("img", {src: "http://127.0.0.1:5502/icon.png", styles: {width: "100%", height: "100%"}});
-        // appicon.src = "https://127.0.0.1:5502/icon.png";
-        icon.styles = {width: "100%", height: "100%", background: "red"}
+    static get appIcon() {
+        let icon = new SvgPlus("img");
+        icon.props = {src: "http://127.0.0.1:5502/images/icon.png", styles: {width: "100%", height: "100%"}};
         return icon;
     }
 }
